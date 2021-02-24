0319ae76bca08687fbb5a626e9471318db066fd6
csaba <csaba@ebi.ac.uk>
2021-02-24 10:12:34 +0000

    Add Ontology Term entity

---
+++ data-model/src/main/java/org/pdxfinder/OntologyTerm.java
@@ -0,0 +1,36 @@
+package org.pdxfinder;
+
+
+import javax.persistence.Entity;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class OntologyTerm {
+
+    private Long id;
+
+    private String url;
+
+    private String label;
+
+    private Set<String> synonyms;
+
+    private String type;
+
+    private String description;
+
+
+
+    public OntologyTerm() {
+    }
+
+    public OntologyTerm(String url, String label) {
+        this.url = url;
+        this.label = label;
+        this.synonyms = new HashSet<>();
+    }
+
+
+
+}
