1ce35629ecbfc71cfcd64ad62bf9927ce67988af
Mauricio Martinez <mauroz77@gmail.com>
2021-02-24 10:44:22 +0000

    Annotation of OntologyTerm class
    - Include lombok in the project.
    - Annotate OntologyTerm class with lombok to have setters/getters and the needed constructors.

--- data-model/src/main/java/org/pdxfinder/OntologyTerm.java
+++ data-model/src/main/java/org/pdxfinder/OntologyTerm.java
@@ -1,37 +1,43 @@
 package org.pdxfinder;
 
-
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
-public class OntologyTerm {
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 
+public class OntologyTerm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NonNull
     private String url;
 
+    @NonNull
     private String label;
 
-    private Set<String> synonyms;
-
+    @NonNull
     private String type;
 
+    @ElementCollection
+    private Set<String> synonyms = new HashSet<>();
+
     private String description;
-
-
-
-    public OntologyTerm() {
-    }
-
-    public OntologyTerm(String url, String label, String type) {
-        this.url = url;
-        this.label = label;
-        this.type = type;
-        this.synonyms = new HashSet<>();
-    }
-
-
 
 }
