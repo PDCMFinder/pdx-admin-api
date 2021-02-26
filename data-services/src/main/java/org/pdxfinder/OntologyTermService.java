bb62269828686f64ca9881e4ebcb80bd83ecf16b
csaba <csaba@ebi.ac.uk>
2021-02-26 13:10:06 +0000

    Refactor service Load treatments and regimens

--- data-services/src/main/java/org/pdxfinder/OntologyTermService.java
+++ data-services/src/main/java/org/pdxfinder/OntologyTermService.java
@@ -22,6 +22,21 @@ public class OntologyTermService {
     private static final String EMBEDDED = "_embedded";
     private static final String OLS_BASE_URL = "https://www.ebi.ac.uk/ols/api/ontologies/ncit/terms/";
 
+    private static final String chemicalModifierBranchUrl = "http://purl.obolibrary.org/obo/NCIT_C1932";
+    private static final String dietarySupplementBranchUrl = "http://purl.obolibrary.org/obo/NCIT_C1505";
+    private static final String drugOrChemByStructBranchUrl = "http://purl.obolibrary.org/obo/NCIT_C1913";
+    private static final String industrialAidBranchUrl = "http://purl.obolibrary.org/obo/NCIT_C45678";
+    private static final String pharmaSubstanceBranchUrl = "http://purl.obolibrary.org/obo/NCIT_C1909";
+    private static final String physiologyBranchUrl = "http://purl.obolibrary.org/obo/NCIT_C1899";
+    private static final String hematopoieticBranchUrl = "http://purl.obolibrary.org/obo/NCIT_C15431";
+    private static final String therapeuticProceduresBranchUrl = "http://purl.obolibrary.org/obo/NCIT_C49236";
+    private static final String clinicalStudyBranchUrl = "http://purl.obolibrary.org/obo/NCIT_C15206";
+    private static final String geneProductBranchUrl = "http://purl.obolibrary.org/obo/NCIT_C26548";
+
+
+    private static final String regimenBranchUrl = "http://purl.obolibrary.org/obo/NCIT_C12218";
+    private static final String customRegimenBranchUrl = "http://purl.obolibrary.org/obo/NCIT_C11197";
+
     Set<String> visitedTerms = new HashSet<>();
     Set<OntologyTerm> toBeSavedTerms = new HashSet<>();
     Set<OntologyTerm> discoveredTerms = new HashSet<>();
@@ -43,6 +58,7 @@ public class OntologyTermService {
         toBeSavedTerms = new HashSet<>();
         ontologyTermRepository.deleteAllByType("diagnosis");
         getDiagnosisTerms();
+        saveOntologyTerms();
         log.info("done");
     }
 
@@ -50,16 +66,66 @@ public class OntologyTermService {
     public void reloadTreatmentTerms(){
         visitedTerms = new HashSet<>();
         discoveredTerms = new HashSet<>();
+        toBeSavedTerms = new HashSet<>();
         ontologyTermRepository.deleteAllByType("treatment");
         ontologyTermRepository.deleteAllByType("regimen");
         getTreatmentTerms();
+        saveOntologyTerms();
+        log.info("done");
     }
 
-    private void getTreatmentTerms(){
+    @Transactional
+    public void getTreatmentTerms(){
+        log.info("Querying treatment and regimen branches in OLS");
+        getTreatmentTermsFromBranch("treatment", chemicalModifierBranchUrl);
+        getTreatmentTermsFromBranch("treatment", dietarySupplementBranchUrl);
+        getTreatmentTermsFromBranch("treatment", drugOrChemByStructBranchUrl);
+        getTreatmentTermsFromBranch("treatment", industrialAidBranchUrl);
+        getTreatmentTermsFromBranch("treatment", pharmaSubstanceBranchUrl);
+        getTreatmentTermsFromBranch("treatment", physiologyBranchUrl);
+        getTreatmentTermsFromBranch("treatment", hematopoieticBranchUrl);
+        getTreatmentTermsFromBranch("treatment", therapeuticProceduresBranchUrl);
+        getTreatmentTermsFromBranch("treatment", clinicalStudyBranchUrl);
+        getTreatmentTermsFromBranch("treatment", geneProductBranchUrl);
+        getTreatmentTermsFromBranch("regimen", regimenBranchUrl);
+        getTreatmentTermsFromBranch("regimen", customRegimenBranchUrl);
 
     }
 
-    private void getDiagnosisTerms(){
+    public void getTreatmentTermsFromBranch(String type, String branchRootUrl){
+
+        int totalPages = 0;
+        boolean totalPagesDetermined = false;
+
+        for (int currentPage = 0; currentPage <= totalPages; currentPage++) {
+
+            String encodedTermUrl = "";
+            try {
+                //have to double encode the url to get the desired result
+                encodedTermUrl = URLEncoder.encode(branchRootUrl, UTF8);
+                encodedTermUrl = URLEncoder.encode(encodedTermUrl, UTF8);
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            String url = OLS_BASE_URL + encodedTermUrl + "/hierarchicalDescendants?size=200&page=" + currentPage;
+            log.info(url);
+            String json = utilityService.parseURL(url);
+            JSONObject job = new JSONObject(json);
+            parseHierarchicalChildren(job, type);
+
+            if (!totalPagesDetermined) {
+                //String page = job.getString("page");
+                JSONObject pageObj = job.getJSONObject("page");
+                    totalPages = pageObj.getInt("totalPages") - 1;
+                    totalPagesDetermined = true;
+                }
+        }
+    }
+
+
+    public void getDiagnosisTerms(){
         log.info("Getting diagnosis terms");
 
         //create the Cancer term
@@ -87,28 +153,30 @@ public class OntologyTermService {
             }
             String url = OLS_BASE_URL+parentUrlEncoded+"/hierarchicalChildren?size=200";
             String json = utilityService.parseURL(url);
-            parseHierarchicalChildren(json, "diagnosis");
-        }
-
-        log.info("Saving {} ontology terms to db", toBeSavedTerms.size());
-        ontologyTermRepository.saveAll(toBeSavedTerms);
-
-    }
-
-    private void parseHierarchicalChildren(String json, String type){
-
-        try {
             JSONObject job = new JSONObject(json);
             //if this term does not have child nodes, continue
-            if (!job.has(EMBEDDED)) return;
+            parseHierarchicalChildren(job, "diagnosis");
+        }
+    }
 
+    public void parseHierarchicalChildren(JSONObject job, String type){
+
+        try {
+
+            if (!job.has(EMBEDDED)) return;
             JSONObject job2 = job.getJSONObject("_embedded");
             JSONArray hierarchicalChildren = job2.getJSONArray("terms");
 
             for (int i = 0; i < hierarchicalChildren.length(); i++) {
 
                 JSONObject term = hierarchicalChildren.getJSONObject(i);
-                OntologyTerm newTerm = createOntologyTerm(term, type);
+                OntologyTerm newTerm = null;
+                if(type.equalsIgnoreCase("diagnosis")) {
+                    newTerm = createDiagnosisOntologyTerm(term);
+                }
+                else{
+                    newTerm = createOntologyTerm(term, type);
+                }
                 if(newTerm != null){
                     toBeSavedTerms.add(newTerm);
                     discoveredTerms.add(newTerm);
@@ -126,13 +194,24 @@ public class OntologyTermService {
         if(visitedTerms.contains(url)) return null;
 
         String termLabel = term.getString("label");
+        OntologyTerm newTerm = new OntologyTerm(url, termLabel, type);
+        String description = "";
+        newTerm.setDescription(description);
+        return newTerm;
+    }
+
+    private OntologyTerm createDiagnosisOntologyTerm(JSONObject term){
+        String url = term.getString("iri");
+        if(visitedTerms.contains(url)) return null;
+
+        String termLabel = term.getString("label");
         String updatedTermLabel = updateTermLabel(termLabel);
 
         termLabel = termLabel.replaceAll(",", "");
 
         OntologyTerm newTerm = new OntologyTerm(
                 url,
-                updatedTermLabel != null ? updatedTermLabel : termLabel, type);
+                updatedTermLabel != null ? updatedTermLabel : termLabel, "diagnosis");
 
         JSONArray synonyms = term.getJSONArray("synonyms");
         Set<String> synonymsSet = new HashSet<>();
@@ -161,6 +240,8 @@ public class OntologyTermService {
         return newTerm;
     }
 
+
+
     private String updateTermLabel(String termLabel){
 
         // Changes Malignant * Neoplasm to * Cancer
@@ -172,6 +253,12 @@ public class OntologyTermService {
             log.trace("Replacing term label '{}' with '{}'", termLabel, updatedTermlabel);
         }
         return updatedTermlabel;
+    }
+
+
+    public void saveOntologyTerms(){
+        log.info("Saving {} ontology terms to db", toBeSavedTerms.size());
+        ontologyTermRepository.saveAll(toBeSavedTerms);
     }
 
 
